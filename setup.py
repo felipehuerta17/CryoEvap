c4c9c72fcd7e48a5d6f51d2ca20a3b0247286740
Felipe Huerta <fnhuerta@uc.cl>
2023-11-05 10:06:13 -0300

    feat: module install configuration files
    add numpy, scipy, matplotlib and CoolProp as requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cryoevap',
+    version='0.1',
+    packages=find_packages(),
+    description='Simulation suite for the evaporation of cryogenic liquids in storage tanks',
+    author='Felipe Huerta',
+    author_email=' fnhuerta@uc.cl',
+)
