02de4dac517036415a79ff4cbf47e958edfee888
Felipe Huerta <fnhuerta@uc.cl>
2024-08-10 11:12:24 -0400

    feat: selectable time units for plotting
    CryoEvap can be used for simulating different tanks across different scales. This new feature allows to show s, min, h or days, depending on the scenario. It is exemplified in the notebooks.

--- cryoevap/storage_tanks/plots.py
+++ cryoevap/storage_tanks/plots.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 
-def plot_tv(tank):
+def plot_tv(tank, t_unit='s'):
     # Number of temperature profiles to visualise
     n_plots = int(tank.sol.t[-1] / tank.plot_interval)
+
+    # Time unit conversion factor for plotting
+    t_dict = {'s':1, 'min': 60, 'h':3600, 'd': 3600*24, 'w': 3600*24*7}
 
     # Step to move in the index
     plot_step = int(tank.plot_interval / tank.time_interval)
 
-    # Create a colormap
+    # Create a colour map
     cmap = plt.get_cmap('cividis')
 
     # Normalize the colormap based on the time range
@@ -26,7 +29,7 @@ def plot_tv(tank):
         ax.plot(T_v, tank.z_grid, color=cmap(norm(tank.sol.t[i * plot_step])))
 
         # Add a text box with the time value at the right of the plot
-        ax.text(1.02, ((i-1 + 0.15)*plot_step) / len(tank.sol.t), f't={tank.sol.t[i*plot_step]:.0f} s', transform=ax.transAxes, 
+        ax.text(1.02, ((i-1 + 0.15)*plot_step) / len(tank.sol.t), f't={tank.sol.t[i*plot_step]/t_dict[t_unit]:.0f} ' + t_unit, transform=ax.transAxes, 
                 verticalalignment='center', bbox=dict(boxstyle='round,pad=0.5', edgecolor='none', facecolor=cmap(norm(tank.sol.t[i*plot_step])), alpha=0.6))
 
     # Add a grid
@@ -37,17 +40,18 @@ def plot_tv(tank):
     ax.set_xlabel('Temperature / K')
 
     # Add a title
-    ax.set_title('Vapour temperature profiles at different times')
+    # ax.set_title('Vapour temperature profiles at different times')
 
     # Show the plot
     # plt.show()
 
-def plot_V_L(tank, unit='m3'):
+def plot_V_L(tank, unit='m3', t_unit='s'):
     '''
     Plots liquid volume
     Inputs:
         Tank object with a sol object produced by the evaporate() function
         unit: Liquid volume units. Default: m3
+        t_unit: Time unit. Default: s
         Options: m^3, L, mL 
     
     Returns:
@@ -57,27 +61,30 @@ def plot_V_L(tank, unit='m3'):
     # Conversion factors for plotting
     unit_conv = {'m3': 1, 'L': 1e3, 'mL': 1e6}
 
+        # Time unit conversion factor for plotting
+    t_dict = {'s':1, 'min': 60, 'h':3600, 'd': 3600*24, 'w': 3600*24*7}
+
     # Create a colormap
     cmap = plt.get_cmap('cividis')
 
-
     # Access to the liquid volume
-    plt.plot(tank.sol.t, tank.sol.y[0] * unit_conv[unit], color = cmap(1/6))
+    plt.plot(tank.sol.t/t_dict[t_unit], tank.sol.y[0] * unit_conv[unit], color = cmap(1/6))
     plt.grid()
-    plt.xlabel('Time / s')
+    plt.xlabel('Time / ' + t_unit)
     if unit == "m3":
         plt.ylabel('$V_L$ / $m^3$')
     else:
         plt.ylabel('$V_L$ / ' + unit)
     return
 
-def plot_BOG(tank, unit='kg/h'):
+def plot_BOG(tank, unit='kg/h', t_unit = 's'):
     '''
     Plots boil-off gas and evaporation rate
 
     Inputs:
         tank: Tank object with a sol object produced by the evaporate() function
         unit: BOG units. Default: kg/h
+        t_unit: Time units. Default: s
         Options: kg/h, kg/s, g/s 
     
     Returns:
@@ -90,19 +97,22 @@ def plot_BOG(tank, unit='kg/h'):
     # Conversion factors for plotting
     unit_conv = {'kg/h': 3600, 'kg/s': 1, 'g/h' : 3600*1e3, 'g/s': 1000}
 
+    # Time unit conversion factor for plotting
+    t_dict = {'s':1, 'min': 60, 'h':3600, 'd': 3600*24, 'w': 3600*24*7}
+
     # Extract evaporation and BOG rates and convert to kg/h
     # Visualise evaporation and boil-off gas rate in kg/h
 
-    plt.plot(tank.sol.t, tank.data['B_L'] * unit_conv[unit], label='Evaporation rate, $\dot{B}_L$', color = cmap(1/6))
-    plt.plot(tank.sol.t[1:], tank.data['BOG'][1:] * unit_conv[unit], label='Boil-off gas rate, $\dot{B}$', color = cmap(5/6)) 
+    plt.plot(tank.sol.t/t_dict[t_unit], tank.data['B_L'] * unit_conv[unit], label='Evaporation rate, $\dot{B}_L$', color = cmap(1/6))
+    plt.plot(tank.sol.t[1:]/t_dict[t_unit], tank.data['BOG'][1:] * unit_conv[unit], label='Boil-off gas rate, $\dot{B}$', color = cmap(5/6)) 
     plt.grid()
-    plt.xlabel('Time / s')
+    plt.xlabel('Time / ' + t_unit)
     plt.ylabel('Mass flow $/$ ' + unit)
     plt.legend()
     plt.show()
     return
 
-def plot_Q(tank, unit='kW'):
+def plot_Q(tank, unit='kW', t_unit = 's'):
     '''
     Plots vapour to liquid heat transfer rate
 
@@ -122,40 +132,44 @@ def plot_Q(tank, unit='kW'):
     # Conversion factors for plotting
     unit_conv = {'W': 1, 'kW': 1e-3}
 
+    # Time unit conversion factor for plotting
+    t_dict = {'s':1, 'min': 60, 'h':3600, 'd': 3600*24, 'w': 3600*24*7}
+
+
     fig, ax = plt.subplots(2, 2, figsize = [6,6])
 
     # Create space to breathe
     plt.subplots_adjust(wspace=0.5)
 
     # Q_L_in plot
-    ax[0][0].plot(tank.sol.t, (tank.data['Q_L']* unit_conv[unit]), color = cmap(1/6))
+    ax[0][0].plot(tank.sol.t/t_dict[t_unit], (tank.data['Q_L']* unit_conv[unit]), color = cmap(1/6))
     ax[0][0].set_ylabel("$\dot{Q}_L$ / " + unit)
-    ax[0][0].set_xlabel("Time / s")
+    ax[0][0].set_xlabel("Time / " + t_unit)
     ax[0][0].grid()
 
     # Q_V_in plot
-    ax[1][0].plot(tank.sol.t, (tank.data['Q_V'] * unit_conv[unit]), color = cmap(1/6))
-    ax[1][0].plot(tank.sol.t, (tank.data['Q_Vw'] * unit_conv[unit]), label="Q_Vw",color = cmap(5/6))
+    ax[1][0].plot(tank.sol.t/t_dict[t_unit], (tank.data['Q_V'] * unit_conv[unit]), color = cmap(1/6))
+    ax[1][0].plot(tank.sol.t/t_dict[t_unit], (tank.data['Q_Vw'] * unit_conv[unit]), label="Q_Vw",color = cmap(5/6))
     ax[1][0].set_ylabel("$\dot{Q}_V$ /  " + unit)
-    ax[1][0].set_xlabel("Time / s")
+    ax[1][0].set_xlabel("Time / " + t_unit)
     ax[1][0].grid()
 
     # Q_VL plot
-    ax[0][1].plot(tank.sol.t, (tank.data['Q_VL'] * unit_conv[unit]), label="Q_VL", color = cmap(1/6))
+    ax[0][1].plot(tank.sol.t/t_dict[t_unit], (tank.data['Q_VL'] * unit_conv[unit]), label="Q_VL", color = cmap(1/6))
     ax[0][1].set_ylabel("$\dot{Q}_{VL}$ / " + unit)
-    ax[0][1].set_xlabel("Time / s")
+    ax[0][1].set_xlabel("Time / " + t_unit)
     ax[0][1].grid()
 
     # Q_{V,w} plot
     #ax[1][1].plot(tank.sol.t, (tank.data['Q_Vw'] * unit_conv[unit]), label="Q_Vw",color = cmap(1/6))
-    ax[1][1].plot(tank.sol.t, ( (tank.data['Q_Vw'] + tank.data['Q_VL'] + tank.data['Q_L'])  *
+    ax[1][1].plot(tank.sol.t/t_dict[t_unit], ( (tank.data['Q_Vw'] + tank.data['Q_VL'] + tank.data['Q_L'] + tank.Q_b)  *
                                 unit_conv[unit]), label="Q_{tot}",color = cmap(1/6))
     # ax[1][1].set_ylabel("$\dot{Q}_{V,w}$ / " + unit)
     ax[1][1].set_ylabel("$\dot{Q}_{tot}$ / " + unit)
-    ax[1][1].set_xlabel("Time / s")
+    ax[1][1].set_xlabel("Time / " + t_unit)
     ax[1][1].grid()
 
-    ax[0][0].set_title("Heat ingresses")
+    # ax[0][0].set_title("Heat ingresses")
 
     # [axis.grid() for axis in ax]
     plt.show()
