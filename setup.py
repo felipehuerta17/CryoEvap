918a5a6be6b8434e4d46f51c93197eb13f509d5d
Felipe Huerta <fnhuerta@uc.cl>
2024-08-10 12:19:34 -0400

    fix: v1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cryoevap',
-    version='0.1',
+    version='1.0',
     packages=find_packages(),
     description='Simulation suite for the evaporation of cryogenic liquids in storage tanks',
     author='Felipe Huerta',
