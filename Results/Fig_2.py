9cb23244f3e46b9975735b7587943f8082fc7165
Ignacio-TG <iptapia@uc.cl>
2025-07-25 14:34:23 -0400

    chore: Figure 2 script created

---
+++ Results/Fig_2.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import pickle as pkl
+
+with open('Data/large_tank_LF95_transient.pkl', 'rb') as f:
+    large_tank = pkl.load(f)
+
+d_i    = 10.24*2     # Internal diameter / m
+d_o    = 10.6972*2   # External diameter / m
+
+r = np.linspace(0, 1, large_tank['T_w_raw'].shape[0]) * (d_o - d_i)/2 + d_i/2
+time_tau = [1, 2, 4, 6, 8, 10]
+
+cmap = plt.get_cmap('inferno', len(time_tau)+1)
+
+plt.figure(figsize= (8,6), dpi=200)
+
+for j, i in enumerate(time_tau):
+    plt.plot(r, large_tank['T_w_raw'][:, i], label=fr't$^*$ = {i/10:.1f}', color = cmap(j))
+
+plt.legend(fontsize = 12)
+plt.xlabel('Radius / m', fontsize=14)
+plt.ylabel('Temperature / K', fontsize=14)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+
+plt.savefig('Figures/Fig_2.svg', bbox_inches='tight', dpi=200)
